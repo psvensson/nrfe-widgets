d9b4c7cbc9026258244139829e16ef04330480d3
Peter Svensson <psvenson@gmail.com>
2015-12-25 16:21:00 +0100

    chabging to correct characteristic handle variable in read char

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ var fn = function (def, parent)
   def.in = function(msg)
   {
     var deviceHandle = msg.payload.device
-    var characteristicHandle = msg.payload.characteristic
+    var characteristicHandle = msg.payload.handle
     var rv = undefined
     if(deviceHandle && characteristicHandle && evothings && evothings.ble)
     {
